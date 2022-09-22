0ce5084e536e4b1589e411a97c4a181f617b81a3
truongdinhhanam <namboyptdtnt293@gmail.com>
2022-09-23 00:21:13 +0700

    first commit

---
+++ Lab6/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LiB_A = new System.Windows.Forms.ListBox();
+            this.LiB_B = new System.Windows.Forms.ListBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // LiB_A
+            // 
+            this.LiB_A.FormattingEnabled = true;
+            this.LiB_A.ItemHeight = 16;
+            this.LiB_A.Items.AddRange(new object[] {
+            "Nguyễn Văn A",
+            "Nguyễn Văn B",
+            "Trần Văn C",
+            "Trần Văn D",
+            "Lê Phước Ý",
+            "Lê Phước Túng"});
+            this.LiB_A.Location = new System.Drawing.Point(127, 195);
+            this.LiB_A.Name = "LiB_A";
+            this.LiB_A.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.LiB_A.Size = new System.Drawing.Size(227, 228);
+            this.LiB_A.TabIndex = 0;
+            // 
+            // LiB_B
+            // 
+            this.LiB_B.FormattingEnabled = true;
+            this.LiB_B.ItemHeight = 16;
+            this.LiB_B.Location = new System.Drawing.Point(590, 195);
+            this.LiB_B.Name = "LiB_B";
+            this.LiB_B.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.LiB_B.Size = new System.Drawing.Size(227, 228);
+            this.LiB_B.TabIndex = 1;
+            this.LiB_B.SelectedIndexChanged += new System.EventHandler(this.LiB_B_SelectedIndexChanged);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(436, 209);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = ">";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(436, 254);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 23);
+            this.btn2.TabIndex = 3;
+            this.btn2.Text = ">>";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(436, 349);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 23);
+            this.btn3.TabIndex = 4;
+            this.btn3.Text = "<";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(436, 387);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 23);
+            this.btn4.TabIndex = 5;
+            this.btn4.Text = "<<";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(127, 52);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(938, 602);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.LiB_B);
+            this.Controls.Add(this.LiB_A);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LiB_A;
+        private System.Windows.Forms.ListBox LiB_B;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Lab6/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LiB_B_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            for(int i = LiB_A.SelectedItems.Count - 1; i>=0; i--)
+            {
+                LiB_B.Items.Add(LiB_A.SelectedItems[i]);
+                LiB_A.Items.Remove(LiB_A.SelectedItems[i]);
+            }
+            LiB_B.Sorted = true;                
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < LiB_A.Items.Count; i++)
+            {
+                LiB_B.Items.Add(LiB_A.Items[i]);
+                
+            }
+            LiB_A.Items.Clear();
+            LiB_B.Sorted = true;
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            for (int i = LiB_B.SelectedItems.Count - 1; i >= 0; i--)
+            {
+                LiB_A.Items.Add(LiB_B.SelectedItems[i]);
+                LiB_B.Items.Remove(LiB_B.SelectedItems[i]);
+            }
+            LiB_A.Sorted = true;
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < LiB_B.Items.Count; i++)
+            {
+                LiB_A.Items.Add(LiB_B.Items[i]);
+
+            }
+            LiB_B.Items.Clear();
+            LiB_A.Sorted = true;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Dictionary<string, string> hoTen = new Dictionary<string, string>();
+            
+        }
+    }
+}
